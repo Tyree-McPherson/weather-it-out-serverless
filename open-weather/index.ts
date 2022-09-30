aefbd0d9f7c46b5438809d0ae3a262d6969a8b90
Tyree-McPherson <Tyree_McPherson@hotmail.com>
2022-09-30 19:19:33 -0400

    updated

--- open-weather/index.ts
+++ open-weather/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const openWeather: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   try {
-    console.log("got here?")
+    
     // Variable declaration.
     const appid = process.env.appid;
     const domain = "https://api.openweathermap.org";
@@ -13,14 +13,10 @@ const openWeather: AzureFunction = async function (context: Context, req: HttpRe
     // Make an API request to the Open Weather API and return the data to the client.
     await axios.get(`${domain}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${appid}`)
     .then((response: any) => context.res!.json(response.data))
-    // .then((response: any) => context.res!.json(response))
     .catch(error => {
       throw error
     })
-    // await fetch(`${domain}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${appid}`)
-    // .then((response: any) => response.json())
-    // .then((response: any) => context.res!.json(response))
-  
+
   } catch (error) {
 
     console.log(error);
